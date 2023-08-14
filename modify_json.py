14dd1beb041937023134aab6702e85ed73bd35bb
Bubbl33s <jairo.lf.8@gmail.com>
2023-08-13 21:36:23 -0500

    Loading db

--- modify_json.py
+++ modify_json.py
@@ -1,39 +1,61 @@
+import firebase_admin
+from firebase_admin import credentials, db
 import json
-import dropbox
+
+print("Iniciando...")
+
+print("Iniciando Firebase...")
+# Iniciar Firebase
+cred = credentials.Certificate('model/token.json')
+firebase_admin.initialize_app(
+    cred, {'databaseURL': 'https://db-for-appointment-sw-default-rtdb.firebaseio.com/'})
+
+# Función para cargar citas desde Firebase a tu JSON local
 
 
-# Initialize dropbox client
-with open("model/token.json", "r") as config:
-    token = json.load(config)
-    dbx = dropbox.Dropbox(token["DROPBOX_TOKEN"])
+def cargar_citas():
+    ref = db.reference('citas')
+    citas = ref.get()
 
-dropbox_path = '/citas_datos_Andy.json'
-local_data_json = 'model/data_local.json'
+    # Guardar en JSON local
+    with open('model/data_local.json', 'w') as f:
+        json.dump(citas, f)
 
-# Configura el archivo local JSON como una lista vacía
-cita_molde = {
-    "id_cita": None,
-    "fecha_prog": None,
-    "hora_prog": None,
-    "ap_pat": None,
-    "ap_mat": None,
-    "nombres": None,
-    "codigo": None,
-    "concepto": None,
-    "obs": None,
-    "flag": None,
-    "estado": None,
-    "telefono": None,
-    "correo": None,
-    "fecha_reg": None,
-    "hora_reg": None
-}
+# Función para guardar una nueva cita en Firebase
 
-# Guardar la lista vacía en el archivo local
-with open(local_data_json, "w") as f:
-    json.dump(cita_molde, f, indent=4)
 
-# Subir el archivo JSON actualizado (ahora vacío) a Dropbox
-with open(local_data_json, "rb") as f:
-    dbx.files_upload(f.read(), dropbox_path,
-                     mode=dropbox.files.WriteMode("overwrite"))
+def guardar_cita(nueva_cita):
+    ref = db.reference('citas')
+
+    # Generar un ID único para la cita. Puedes usar otras estrategias para generar la ID.
+    id_cita = ref.push().key
+
+    # Guardar la cita en Firebase
+    ref.child(id_cita).set(nueva_cita)
+
+
+def obtener_key_por_id_cita(id_cita_buscado):
+    ref = db.reference('citas')
+    citas = ref.get()
+
+    for key, cita in citas.items():
+        if cita['id_cita'] == id_cita_buscado:
+            return key
+    return None  # Si no se encuentra el id_cita
+
+
+def eliminar_cita_por_id_cita(id_cita_buscado):
+    key = obtener_key_por_id_cita(id_cita_buscado)
+    if key:
+        ref = db.reference(f'citas/{key}')
+        ref.delete()
+        print(f"Cita con id_cita {id_cita_buscado} eliminada.")
+    else:
+        print(f"No se encontró ninguna cita con id_cita {id_cita_buscado}.")
+
+
+# Ejemplo de uso:
+eliminar_cita_por_id_cita("002")
+
+# Cargar citas
+cargar_citas()
