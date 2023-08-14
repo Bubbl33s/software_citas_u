50a04c5d6f4bd52f50f246fedfbbec57fa08a78a
Bubbl33s <jairo.lf.8@gmail.com>
2023-08-13 23:52:10 -0500

    main window sizing methods

--- view/main_window.py
+++ view/main_window.py
@@ -11,58 +11,88 @@ class VentanaPrincipal(QMainWindow):
         # Cargar la interfaz desde el archivo .ui
         loadUi("ui/main_window.ui", self)
 
+        self.set_fw_highlight()
         # Conectar el botón btn_edit al método toggle_float_window
         self.btn_edit.clicked.connect(self.toggle_float_window)
+        self.adjust_table_widget()
 
-        # Guardar las proporciones iniciales del botón btn_edit
-        self.proporcion_x_btn = self.btn_edit.x() / self.width()
-        self.proporcion_y_btn = self.btn_edit.y() / self.height()
-
+# WIDGETS SETTER AND SIZING METHODS---------------------------------------------------------------------------------------
+    def set_fw_highlight(self):
+        self.float_window.setParent(self.main_container)
         # Inicializar el efecto de desenfoque
         self.blur_effect = QGraphicsBlurEffect()
         self.blur_effect.setBlurRadius(0)  # Sin desenfoque al inicio
         self.container.setGraphicsEffect(self.blur_effect)
-
         # Inicializar el overlay para bloquear interacciones y oscurecer
         # Colocado en main_container
         self.overlay = QLabel(self.main_container)
         self.overlay.setGeometry(
             0, 0, self.main_container.width(), self.main_container.height())
-        self.overlay.setStyleSheet("background-color: rgba(0, 0, 0, 40);")
+
+        self.overlay.setStyleSheet(
+            "background-color: rgba(0, 0, 0, 40); border-radius: 20px;")
         self.overlay.hide()
         self.float_window.hide()
 
-    def resizeEvent(self, event):  # <-- Asegúrate de que 'E' esté en mayúsculas
+    def resize_float_window(self):
+        # Ajustar el tamaño y posición de float_window para que esté centrado
+        width_float_window = 800
+        height_float_window = 600
+        x_float_window = (self.main_container.width() - width_float_window) / 2
+        y_float_window = (self.main_container.height() -
+                          height_float_window) / 2
+        self.float_window.setGeometry(
+            int(x_float_window), int(y_float_window), width_float_window, height_float_window)
+
+    def resize_containers(self):
         # Ajustar el tamaño y posición del main_container
-        margin = 20
+        self.main_container_margin = 20
         self.main_container.setGeometry(
-            margin, margin, self.width() - 2*margin, self.height() - 2*margin)
+            self.main_container_margin, self.main_container_margin,
+            self.width() - 2*self.main_container_margin,
+            self.height() - 2*self.main_container_margin)
 
         # Ajustar el tamaño y posición de container para que se mantenga proporcional
-        margin_container = 20
+        # Margen a 0 para que se puedan poner los widgets al borde
         self.container.setGeometry(
-            margin_container, margin_container,
-            self.main_container.width() - 2*margin_container,
-            self.main_container.height() - 2*margin_container)
+            0, 0, self.main_container.width(), self.main_container.height())
 
-        # Ajustar el tamaño y posición de float_window
-        ancho_float_window = 800
-        alto_float_window = 600
-        x_float_window = int(
-            (self.main_container.width() - ancho_float_window) / 2)
-        y_float_window = int(
-            (self.main_container.height() - alto_float_window) / 2)
-        self.float_window.setGeometry(
-            x_float_window, y_float_window, ancho_float_window, alto_float_window)
-
+    def resize_buttons(self):
         # Ajustar posición de btn_edit basándose en las proporciones iniciales
-        x_btn = self.width() * self.proporcion_x_btn
-        y_btn = self.height() * self.proporcion_y_btn
+        x_btn = self.width() * self.prop_x_btn
+        y_btn = self.height() * self.prop_y_btn
         self.btn_edit.move(int(x_btn), int(y_btn))
+        # TODO: ADD MORE BUTTONS
+
+    def get_buttons_prop(self):
+        # Guardar las proporciones iniciales del botón btn_edit
+        self.prop_x_btn = self.btn_edit.x() / self.width()
+        self.prop_y_btn = self.btn_edit.y() / self.height()
+        # TODO: ADD MORE BUTTONS
+
+    def resizeEvent(self, event):
+        self.resize_containers()
+        self.resize_float_window()
 
         # Ajustar el tamaño del overlay
         self.overlay.setGeometry(0, 0, self.main_container.width(),
                                  self.main_container.height())
+
+        self.adjust_table_widget()
+
+    def adjust_table_widget(self):
+        self.table_margin = 20  # Margen uniforme (lateral, inferior)
+        self.table_top_offset = 200  # Separación desde la parte superior
+
+        table_width = self.container.width() - 2 * self.table_margin
+        table_height = self.container.height() - self.table_top_offset - \
+            self.table_margin
+
+        # Cálculo para centrar horizontalmente
+        table_x_position = int((self.container.width() - table_width) / 2)
+
+        self.tbl_appoinment.setGeometry(
+            table_x_position, self.table_top_offset, table_width, table_height)
 
     def toggle_float_window(self):
         if self.float_window.isVisible():
@@ -70,12 +100,23 @@ class VentanaPrincipal(QMainWindow):
             self.overlay.hide()
             self.blur_effect.setBlurRadius(0)  # Sin desenfoque
         else:
+            # Ajusta el nivel de desenfoque según prefieras
+            self.blur_effect.setBlurRadius(10)
+
+            width_float_window = 800
+            height_float_window = 600
+            x_float_window = (self.main_container.width() -
+                              width_float_window) / 2
+            y_float_window = (self.main_container.height() -
+                              height_float_window) / 2
+            self.float_window.setGeometry(
+                int(x_float_window), int(y_float_window), width_float_window, height_float_window)
+
             self.float_window.show()
             self.overlay.show()
             self.overlay.raise_()  # Asegurar que overlay esté por debajo de float_window
             self.float_window.raise_()  # Asegurar que float_window esté encima
-            # Ajusta el nivel de desenfoque según prefieras
-            self.blur_effect.setBlurRadius(10)
+# ------------------------------------------------------------------------------------------------------------------------
 
 
 if __name__ == '__main__':
